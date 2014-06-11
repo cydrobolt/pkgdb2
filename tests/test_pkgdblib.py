dfe620c6929532e4b244990190641abf5ab88174
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-06-11 17:24:02 +0200

    Expand the test for search_actions

--- tests/test_pkgdblib.py
+++ tests/test_pkgdblib.py
@@ -1629,7 +1629,9 @@ class PkgdbLibtests(Modeltests):
         # Check after insert
         actions = pkgdblib.search_actions(
             self.session,
-            package='guake'
+            package='guake',
+            page=1,
+            limit=50,
         )
         self.assertEqual(len(actions), 1)
         self.assertEqual(actions[0].user, 'pingou')
